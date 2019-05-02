fc9d967069d35750d4e2754df8720f4ef530045c
Aleixxor <alexlouzada2009@gmail.com>
2019-05-02 20:55:24 -0300

    Delete user.js

--- Client/classes/user.js
+++
@@ -1,8 +0,0 @@
-export default class User {
-  constructor(nome, tipoUsuario, email, password) {
-      this.nome = nome;
-      this.tipoUsuario = tipoUsuario;
-      this.email = email;
-      this.password = password;
-  }
-}
