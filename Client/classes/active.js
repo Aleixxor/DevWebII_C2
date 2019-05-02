87cf261c7937c31de04f038901f2582e3d729359
Aleixxor <alexlouzada2009@gmail.com>
2019-05-02 20:55:12 -0300

    Delete active.js

--- Client/classes/active.js
+++
@@ -1,8 +0,0 @@
-export default class Active {
-    constructor(cod_ativo, __cod_usuario, descricao, empresa) {
-        this.cod_ativo = cod_ativo;
-        this.__cod_usuario = __cod_usuario;
-        this.descricao = descricao;
-        this.empresa = empresa;
-    }
-}
